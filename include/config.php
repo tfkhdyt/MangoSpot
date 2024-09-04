ffd77c1542515c5410e64ecd45b41fb529e1862a
Taufik Hidayat <tfkhdyt@proton.me>
2024-09-04 11:01:42 +0700

    fix(config): use environment variables for database connection
    This commit changes the configuration to use environment variables for database connection, instead of hard-coding them in the config file.
    
    This allows for more flexibility and security in managing database connections.

--- include/config.php
+++ include/config.php
@@ -1,14 +1,13 @@
 <?php
 define("TYPE", "mysql"); // pgsql or mysql
-define("DB_HOST", "localhost"); // localhost or ip
-define("DB_USER", "username"); // username database
-define("DB_PASSWORD", "password"); // password database
-define("DB_DATABASE", "database"); // table database
+define("DB_HOST", getenv("DB_HOST")); // localhost or ip
+define("DB_USER", getenv("DB_USER")); // username database
+define("DB_PASSWORD", getenv("DB_PASSWORD")); // password database
+define("DB_DATABASE", getenv("DB_DATABASE")); // table database
 define("SSH_HOST", $_SERVER['SERVER_NAME']); // SSH Host
 define("SSH_PORT", 22); // Port SSH
 define("SSH_USERNAME", "ssh_username"); // Username SSH
 define("SSH_PASSWORD", "ssh_password"); // Password SSH
-if ( !defined('ABSPATH') )
+if (!defined('ABSPATH'))
 	define('ABSPATH', dirname(__FILE__) . '/');
 require_once(ABSPATH . 'connect.php');
-?>
